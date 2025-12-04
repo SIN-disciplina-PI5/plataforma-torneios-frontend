aa4f300128d26a3561629c36c6c2e2562e1c5d7b
Gabriel Santos <163359098+gabssiin@users.noreply.github.com>
2025-12-04 14:08:42 +0000

    feat: editando tela meu perfil

--- torneio-app/src/app/user/profile/index.tsx
+++ torneio-app/src/app/user/profile/index.tsx
@@ -5,7 +5,7 @@ import { View, Text } from "react-native";
 export default function userPerfil() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Perfil</Text>
+            <Text>Tela meu Perfil</Text>
         </View>
     );
 }
