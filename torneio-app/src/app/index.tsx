d7be811d4df65c8ce82466c7248e09c7556af889
Mel Lopes <163359225+mellopes06br@users.noreply.github.com>
2025-12-04 22:50:48 +0000

    criação da tela de editar partidas PUT

--- torneio-app/src/app/index.tsx
+++ torneio-app/src/app/index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { View, Text, Image, StyleSheet, SafeAreaView, ScrollView, Pressable, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
