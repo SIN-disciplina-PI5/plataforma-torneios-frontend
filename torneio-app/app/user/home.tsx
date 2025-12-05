55e610b4d7ffec2ee9fcb2cddc060d2215a1af1f
Pedro Delmiro <163359112+PedroDelmiro13@users.noreply.github.com>
2025-12-05 21:10:04 +0000

    adicionando home pra passar da parte de login

--- torneio-app/app/user/home.tsx
+++ torneio-app/app/user/home.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-// Importa o componente da barra de navegação
 import NavBar from "../../components/BarraNavegacao"; 
 
 
 export default function UserHome() {
     return (
-        // O estilo 'flex: 1' garante que a tela ocupe todo o espaço
         <View style={styles.container}>
-            {/* Conteúdo principal da tela */}
             <View style={styles.content}>
                 <Text style={styles.title}>Bem-vindo ao App!</Text>
                 <Text style={styles.subtitle}>Sua Home está aqui.</Text>
-                {/* Adicione mais conteúdo da tela principal aqui, se necessário. */}
             </View>
             
-            {/* O componente NavBar será fixado na parte inferior devido aos seus estilos internos */}
             <NavBar />
         </View>
     );
@@ -23,14 +18,14 @@ export default function UserHome() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, // Preenche toda a tela
+        flex: 1, 
         backgroundColor: '#f9f9f9',
     },
     content: {
         flex: 1,
         justifyContent: 'center', 
         alignItems: 'center',
-        paddingBottom: 70, // Adiciona padding para que o conteúdo não fique escondido pela NavBar
+        paddingBottom: 70, 
     },
     title: {
         fontSize: 24,
