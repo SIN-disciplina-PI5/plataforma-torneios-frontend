d9d77b890bafa0d25024ec11b7717dac51e522a8
mellopes06br <melferreira1808@gmail.com>
2026-03-30 22:00:36 -0300

    feat: tela de abertura criada e arquitetura correta criada, chore: elemento.png adicionado

--- arena-lagoa-beach/app/(private)/home/page.tsx
+++ arena-lagoa-beach/app/(private)/home/page.tsx
@@ -3,6 +3,7 @@ export default function HomePage() {
     <div>
       <h1 className="text-4xl font-bold mb-4 text-[#2d2d2d]">Dashboard
         (Home  GET  partidas USER)
+        
       </h1>
       </div>
   );
