de23b0e8bcbfa0520bb48d13e34cd308ced36297
Pedro Delmiro <163359112+PedroDelmiro13@users.noreply.github.com>
2026-04-22 16:13:56 +0000

    fix: corrigindo path de integração

--- arena-lagoa-beach/app/services/password.ts
+++ arena-lagoa-beach/app/services/password.ts
@@ -1,11 +1,11 @@
 import { api } from "./api"; 
 
 export const forgotPassword = async (email: string) => {
-  const response = await api.post("/forgot-password", { email });
+  const response = await api.post("/password/forgot-password", { email });
   return response.data; 
 };
 
 export const resetPassword = async (token: string, novaSenha: string) => {
-  const response = await api.post("/reset-password", { token, novaSenha });
+  const response = await api.post("/password/reset-password", { token, novaSenha });
   return response.data; 
 };
