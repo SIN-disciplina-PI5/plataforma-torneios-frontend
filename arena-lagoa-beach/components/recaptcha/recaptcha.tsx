5af9b3478ea3ae302d9d939d2b696a9345255a7b
Kaiki Barros <kaikibarros2004@gmail.com>
2026-04-23 23:05:49 -0300

    chore: test recaptcha

--- arena-lagoa-beach/components/recaptcha/recaptcha.tsx
+++ arena-lagoa-beach/components/recaptcha/recaptcha.tsx
@@ -7,9 +7,12 @@ type RecaptchaProps = {
 };
 // Adiciona o componente ReCAPTCHA do Google para proteger o formulário contra bots.
 export default function Recaptcha({ onChange }: RecaptchaProps) {
+  // impressão do site key para verificar se está sendo carregado corretamente, deve ser removida em produção
+    console.log("SITE KEY:", process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY);
   return (
     <ReCAPTCHA
-      sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+    // teste para o sitekey, deve ser substituído pelo sitekey real do reCAPTCHA para produção
+      sitekey="6LfBKbssAAAAAP_r-5vkQwcRGWP4Fglr5YsaRBRE"
       onChange={onChange}
     />
   );
